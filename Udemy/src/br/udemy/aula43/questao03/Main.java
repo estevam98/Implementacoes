60c625b60da4bbff5d9a47b3ca08733f719ccfd9
Jhonatan Martins Estevam <jhonatanm222@gmail.com>
2017-04-03 13:54:03 -0300

    Anything is wrong

---
+++ Udemy/src/br/udemy/aula43/questao03/Main.java
@@ -0,0 +1,13 @@
+package br.udemy.aula43.questao03;
+
+import java.awt.Rectangle;
+
+public class Main {
+	public static void main(String[] args) {
+		Rectangle myrec;
+		myrec.width = 40;
+		myrec.height = 50;
+		
+		System.out.println("MyRec's area is " + myrec.area());
+	}
+}
