57d17509811025f863a9c39b98ce6245ba0adfd0
Jhonatan Martins Estevam <jhonatanm222@gmail.com>
2017-08-03 17:25:07 -0300

    feature edit

--- jsf-devmedia/src/main/java/br/devmedia/bean/LembreteBean.java
+++ jsf-devmedia/src/main/java/br/devmedia/bean/LembreteBean.java
@@ -62,6 +62,19 @@ public class LembreteBean {
 		}
 	}
 	
+	public String edit(){
+		mapper.edit(lembrete);
+		
+		lembrete = new Lembrete();
+		
+		FacesContext context = FacesContext.getCurrentInstance();
+		context.addMessage(null, new FacesMessage("Lembrete editado com sucesso !"));
+		context.getExternalContext().getFlash().setKeepMessages(true);
+		
+		
+		return "index.xhtml?faces-redirect=true";
+	}
+	
 	@PostConstruct
 	public void init(){
 		lembrete = new Lembrete();
