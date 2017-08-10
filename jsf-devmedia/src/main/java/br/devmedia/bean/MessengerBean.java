34c1cddf481e24cdaba039fa0b1d03072ec3fdbc
Jhonatan Martins Estevam <jhonatanm222@gmail.com>
2017-08-10 17:32:26 -0300

    faces-config.xml and redirect

--- jsf-devmedia/src/main/java/br/devmedia/bean/MessengerBean.java
+++ jsf-devmedia/src/main/java/br/devmedia/bean/MessengerBean.java
@@ -1,17 +1,30 @@
 package br.devmedia.bean;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
-import br.devmedia.model.Messenger;
-
-@ManagedBean
+@ManagedBean(name = "messengerBean")
 @SessionScoped
 public class MessengerBean {
 	
-	private Messenger messenger;
+	private String messenger;
 	
-	public Messenger getMessenger(){
+	public String getMessenger(){
 		return messenger;
+	}
+
+	public MessengerBean() {
+		SimpleDateFormat date = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss:S");
+		
+		messenger = "A aplicação foi ao ar as:" + date.format(Calendar.getInstance().getTime());
+	}
+	
+	public String sobre(){
+		messenger = "Redirecionamento feito com sucesso";
+		
+		return "sobre?faces-redirect=true";
 	}
 }
